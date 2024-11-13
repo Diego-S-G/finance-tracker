7a6dee9ed438b83a9b7191827a6c64333511e912
Diego-S-G <diego07goncalves@gmail.com>
2024-11-13 14:29:34 -0300

    Testa as funcionalidades de criar conta, logar e cria o link para deslogar, resolvendo o problema com o get em vez de delete ao tentar deslogar e dar destroy na session.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root 'welcome#index'
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
