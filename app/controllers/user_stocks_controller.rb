f61a98933607ea723fa333dd098fb3eab47f9abd
Diego-S-G <diego07goncalves@gmail.com>
2024-11-15 14:59:13 -0300

    Faz o botão de remover apontar para o modal específico daquele registro, em vez do mesmo modal como estava acontecendo antes. Implementa remoção no controller.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,6 +11,10 @@ class UserStocksController < ApplicationController
   end
 
   def destroy
-    
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} foi removido do portfólio com sucesso."
+    redirect_to my_portfolio_path
   end
 end
