7a767a403c3b06156ba859c8125f1ca8f237e3fa
Diego-S-G <diego07goncalves@gmail.com>
2024-11-15 10:57:06 -0300

    Limpa navbar mostrando links e botões quando logado, com rotas, e limpa em si em questão de estilo algumas páginas, além de consertar erros.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,7 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
+        @tracked_stocks = current_user.stocks
         render 'users/my_portfolio'
       else
         flash[:alert] = 'Por favor insira uma sigla válida.'
@@ -18,6 +19,7 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.find_by(ticker: params[:stock])
       if @stock
+        @tracked_stocks = current_user.stocks
         render 'users/my_portfolio'
       else
         flash[:alert] = 'Por favor insira uma sigla válida.'
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,7 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker, presense: true
+  validates :name, :ticker, presence: true
 
   def self.company_lookup(ticker_symbol)
     response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage_client[:alpha_api_key]}")
