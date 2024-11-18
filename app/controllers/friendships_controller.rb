311d23e9f2827f4e8507470716d5c6d8da253c47
Diego-S-G <diego07goncalves@gmail.com>
2024-11-18 12:32:04 -0300

    Implementa funcionalidade de seguir usuário.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = "Você está seguindo #{friend.full_name}!"
+    else
+      flash[:alert] = "Algo de errado aconteceu..."
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
